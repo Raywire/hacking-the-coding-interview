840950973bc8414c34b33ab955b9bfb9eb861348
Ryan Wire <ryanwire@outlook.com>
2022-05-21 01:25:23 +0300

    Adds recursive binary search

--- binary-search/index.js
+++ binary-search/index.js
@@ -18,4 +18,31 @@ let binarySearch = function(nums, target) {
   return -1;
 };
 
-module.exports = binarySearch;
+let binarySearchRecursive = function(nums, target, low = 0, high = (nums.length - 1)) {
+  if (low > high) {
+    return -1;
+  }
+  
+  // Finding the mid using floor division
+  let mid = low + Math.floor((high - low) / 2);
+  
+  // Target value is present at the middle of the array
+  if (nums[mid] === target) {
+    return mid;
+  } 
+
+  // Target value is present in the low subarray
+  else if (target < nums[mid]) {
+    return binarySearchRecursive(nums, target, low, (mid - 1));
+  } 
+  
+  // Target value is present in the high subarray
+  else {
+    return binarySearchRecursive(nums, target, (mid + 1), high);
+  }
+};
+
+module.exports = {
+  binarySearch,
+  binarySearchRecursive
+}
--- binary-search/index.test.js
+++ binary-search/index.test.js
@@ -1,4 +1,4 @@
-const binarySearch = require('./index')
+const { binarySearch, binarySearchRecursive }= require('./index')
 
 test('checks binary search works', () => {
   expect(binarySearch([], 12)).toBe(-1);
@@ -18,4 +18,24 @@ test('checks binary search works', () => {
 
 test('checks binary search works', () => {
   expect(binarySearch([-1, 0, 3, 5, 9, 12], 2)).toBe(-1);
+});
+
+test('checks recursive binary search works', () => {
+  expect(binarySearchRecursive([], 12)).toBe(-1);
+});
+
+test('checks recursive binary search works', () => {
+  expect(binarySearchRecursive([0, 1], 1)).toBe(1);
+});
+
+test('checks recursive binary search works', () => {
+  expect(binarySearchRecursive([1, 2, 3], 3)).toBe(2);
+});
+
+test('checks recursive binary search works', () => {
+  expect(binarySearchRecursive([-1, 0, 3, 5, 9, 12], 9)).toBe(4);
+});
+
+test('checks recursive binary search works', () => {
+  expect(binarySearchRecursive([-1, 0, 3, 5, 9, 12], 2)).toBe(-1);
 });
