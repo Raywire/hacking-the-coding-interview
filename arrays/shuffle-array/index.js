1b89009898b846df5e0a7dc154feed65dd3b455e
Ryan Wire <ryanwire@outlook.com>
2022-06-18 03:15:20 +0300

    Updates shuffle array solution

--- arrays/shuffle-array/index.js
+++ arrays/shuffle-array/index.js
@@ -4,7 +4,7 @@ class Solution {
       this.n = nums.length;
   }
   reset() {
-      return this.nums;
+      return this.original;
   }
   shuffle() {
       let shuffled = [];
@@ -30,6 +30,55 @@ class Solution {
       return shuffled;
   }
 }
+
+/**
+ * @param {number[]} nums
+ */
+var Solution2 = function(nums) {
+  this.original = nums;
+  this.n = nums.length;
+};
+
+/**
+* @return {number[]}
+*/
+Solution.prototype.reset = function() {
+  return this.original;
+};
+
+/**
+* @return {number[]}
+*/
+Solution.prototype.shuffle = function() {
+    let shuffled = [];
+    // make a copy of the original
+    this.original.forEach((v, index) => shuffled[index] = v);  
+    
+    // initialize unshuffled left part size
+    let leftSize = this.n;
+
+    for (let i = 0; i < shuffled.length; i++) {
+        // draw from the bag at random
+        let j = Math.floor(Math.random() * leftSize) + i;
+        
+        // put this element at current position
+        // and put the original element in the bag
+        let tmp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = tmp;
+        
+        // reduce the size of the un-shuffled part of the array
+        leftSize--;
+    }
+    return shuffled;
+};
+
+/** 
+* Your Solution object will be instantiated and called as such:
+* var obj = new Solution(nums)
+* var param_1 = obj.reset()
+* var param_2 = obj.shuffle()
+*/
 
 module.exports = {
   Solution
